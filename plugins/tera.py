002b22edf831968edcf1645b1752f62b05ad4645
ayan9605 <sumit334434@gmail.com>
2026-05-10 19:33:13 +0530

    Update tera.py

--- plugins/tera.py
+++ plugins/tera.py
@@ -34,7 +34,7 @@ API_BASE_URL = "https://gold-newt-367030.hostingersite.com/tera.php"
 BROWSER_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Encoding": "gzip, deflate",
     "Accept-Language": "en-US,en;q=0.9",
     "Connection": "keep-alive",
     "Sec-Fetch-Dest": "document",
